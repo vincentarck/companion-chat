d54dec5015b85366af152c94e03f284fc6a672dc
Vincent laurensius <vincenthambaya@gmail.com>
2023-09-12 12:31:10 +0700

    [UI] Categories for filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { UserButton } from "@clerk/nextjs";
 import SearchInput from '@/components/SearchInput';
+import prismadb from '@/lib/prismadb';
+import Categories from '@/components/Categories';
 
-export default function RootPage() {
+export default async function RootPage() {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className='h-full p-4'>
-      <SearchInput />
+      <SearchInput  />
+      <Categories data={categories} />
     </div>
   )
 }
