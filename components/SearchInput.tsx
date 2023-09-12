a8fc09dd7eee4af9dc83752dbfa20fc824870743
Vincent laurensius <vincenthambaya@gmail.com>
2023-09-12 11:32:16 +0700

    [FEAT] Integrate input change

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { ChangeEventHandler, useEffect, useState } from 'react'
 import { Input } from "@/components/ui/input"
 
 import { Search } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
-
+import useDebounce from '@/hooks/useDebounce';
+import qs from "query-string"
 export default function SearchInput() {
 
     const router = useRouter();
@@ -15,13 +16,33 @@ export default function SearchInput() {
     const categoryId = searchParams.get('categoryId');
     const name = searchParams.get("name")
 
-    
-    const [value, setValue] = useState(name || "")
 
-  return (
-    <div className='relative'>
-        <Search className='absolute h-4 w-4 top-[calc((40px-16px)/2)] left-4'/>
-        <Input placeholder='Search ...' className='pl-10 b-0 bg-secondary/10'/>
-    </div>
-  )
+    const [value, setValue] = useState(name || "")
+    const debouncedInput = useDebounce<string>(value);
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setValue(e.target.value)
+    }
+
+    useEffect(() => {
+        const query = {
+            name: debouncedInput,
+            categoryId
+        }
+
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query
+        })
+
+        router.push(url)
+
+    }, [debouncedInput, router, categoryId])
+
+    return (
+        <div className='relative'>
+            <Search className='absolute h-4 w-4 top-[calc((40px-16px)/2)] left-4' />
+            <Input onChange={onChange} placeholder='Search ...' className='pl-10 b-0 bg-secondary/10' />
+        </div>
+    )
 }
